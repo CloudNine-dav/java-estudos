caf0963d0a0f015bc8f65f377fab51e376b7eb60
CloudNine-dav <davisoaresdeoliveira@gmail.com>
2026-09-04 11:18:47 -0300

    POO (curso): getters, setters e construtor na classe

---
+++ src/Caneta04Main.java
@@ -0,0 +1,8 @@
+public class Caneta04Main {
+    public static void main(String[] args) {
+        Caneta04 c1 = new Caneta04("bic", "laranja", 0.7f);
+        c1.status();
+        Caneta04 c2 = new Caneta04("nic", "vermelho", 0.5f);
+        c2.status();
+    }
+}
